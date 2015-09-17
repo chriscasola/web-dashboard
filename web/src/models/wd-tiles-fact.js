be210086b62beee676d1e2ae0cb82e2ee3602de6
Chris Casola <chriscasola@gmail.com>
2015-09-16 20:56:13 -0400

    feat(tiles): add a default link and image url to link tiles

--- web/src/models/wd-tiles-fact.js
+++ web/src/models/wd-tiles-fact.js
@@ -1,5 +1,17 @@
-function wdTileFact($http, smModelFactory) {
-  const model = smModelFactory('/tiles/:id').done();
+function wdTileFact($http, smModelFactory, SMModelInstance) {
+  class TileModelInstance extends SMModelInstance {
+    constructor(config) {
+      super(config);
+      if (!this.props.imageUrl && !this.props.link) {
+        this.props.imageUrl = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+        this.props.link = 'http://www.google.com/';
+      }
+    }
+  }
+
+  const model = smModelFactory('/tiles/:id')
+    .modelInstance(TileModelInstance)
+    .done();
 
   return {
     list: function() {
@@ -11,6 +23,6 @@ function wdTileFact($http, smModelFactory) {
   };
 }
 
-wdTileFact.$inject = ['$http', 'smModelFactory'];
+wdTileFact.$inject = ['$http', 'smModelFactory', 'SMModelInstance'];
 
 angular.module('web-dashboard').factory('wdTiles', wdTileFact);
